de7c57abcd737b4e5847ea4796cac24dcc9af596
Marcela Moura <marcelamoura@MacBook-Air-de-MJ.local>
2019-11-12 17:30:06 -0300

    my first commit

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,53 @@
+class ArticlesController < ApplicationController
+
+	http_basic_authenticate_with name: "msm", password: "123", except: [:index, :show]
+
+	def index
+		@articles = Article.all
+	end
+
+	def show
+		@article = Article.find(params[:id]) #para achar oparametro passado entre []
+	end
+
+	def new
+		@article = Article.new
+	end
+
+	def edit
+		@article = Article.find(params[:id])
+	end
+
+	def create
+		@article = Article.new(article_params) #relembra quw contem os atributos que nos interesa
+			
+		if @article.save #return a bullean t or f (save or not)
+			redirect_to @article
+		else
+			render 'new'
+		end
+	end
+
+	def update
+		@article = Article.find(params[:id])
+
+		if @article.update(article_params)
+			redirect_to @article
+		else
+			render 'edit'	
+		end
+	end
+
+	def destroy
+		@article = Article.find(params[:id])
+		@article.destroy
+
+		redirect_to articles_path
+	end
+
+	private
+		def article_params
+			params.require(:article).permit(:title, :text, :summary)
+			#STRONG PARAMETERS(several security)
+		end
+end
---
+++ app/models/comment.rb
@@ -0,0 +1,3 @@
+class Comment < ApplicationRecord
+  belongs_to :article #active recorder association
+end
